145e9aba9ade71eac6ade83068d9a86759fd0ced
yogeshwar607 <yogeshwartanwar@yogi-mac.cybage.com>
2016-06-15 18:43:17 +0530

    home page ui improved

--- InstaRemDemo/platforms/ios/build/InstaRemDemo.build/Debug-iphonesimulator/InstaRemDemo.build/Objects-normal/i386/main.d
+++ InstaRemDemo/platforms/ios/build/InstaRemDemo.build/Debug-iphonesimulator/InstaRemDemo.build/Objects-normal/i386/main.d
@@ -1,8 +1,8 @@
 dependencies: \
-  /Users/yogismac/CodeBase/NativeScript/instarem-demo/InstaRemDemo/platforms/ios/internal/main.m \
-  /Users/yogismac/CodeBase/NativeScript/instarem-demo/InstaRemDemo/platforms/ios/internal/NativeScript/include/NativeScript.h \
-  /Users/yogismac/CodeBase/NativeScript/instarem-demo/InstaRemDemo/platforms/ios/internal/NativeScript/include/TNSRuntime.h \
-  /Users/yogismac/CodeBase/NativeScript/instarem-demo/InstaRemDemo/platforms/ios/internal/NativeScript/include/TNSRuntime+Diagnostics.h \
-  /Users/yogismac/CodeBase/NativeScript/instarem-demo/InstaRemDemo/platforms/ios/internal/NativeScript/include/TNSRuntime+Inspector.h \
-  /Users/yogismac/CodeBase/NativeScript/instarem-demo/InstaRemDemo/platforms/ios/internal/TNSExceptionHandler.h \
-  /Users/yogismac/CodeBase/NativeScript/instarem-demo/InstaRemDemo/platforms/ios/internal/TNSDebugging.h
+  /Users/yogeshwartanwar/Documents/insta-rm/InstaDemo/InstaRemDemo/platforms/ios/internal/main.m \
+  /Users/yogeshwartanwar/Documents/insta-rm/InstaDemo/InstaRemDemo/platforms/ios/internal/NativeScript/include/NativeScript.h \
+  /Users/yogeshwartanwar/Documents/insta-rm/InstaDemo/InstaRemDemo/platforms/ios/internal/NativeScript/include/TNSRuntime.h \
+  /Users/yogeshwartanwar/Documents/insta-rm/InstaDemo/InstaRemDemo/platforms/ios/internal/NativeScript/include/TNSRuntime+Diagnostics.h \
+  /Users/yogeshwartanwar/Documents/insta-rm/InstaDemo/InstaRemDemo/platforms/ios/internal/NativeScript/include/TNSRuntime+Inspector.h \
+  /Users/yogeshwartanwar/Documents/insta-rm/InstaDemo/InstaRemDemo/platforms/ios/internal/TNSExceptionHandler.h \
+  /Users/yogeshwartanwar/Documents/insta-rm/InstaDemo/InstaRemDemo/platforms/ios/internal/TNSDebugging.h
